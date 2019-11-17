28cb0bb53d9293efead50e7854a5577fafd68255
Ivan <ivanross.me@gmail.com>
2019-11-17 18:06:57 +0100

    add some tests

--- test.js
+++ test.js
@@ -1,7 +1,35 @@
 const { continuousArray } = require('./dist/index')
-const array = [1, 2, 4]
-const continuous = continuousArray(array)
 
-console.log(continuous[0])
-console.log(continuous[0.5])
-console.log(continuous[1.5])
+test('interpolation works', () => {
+	const array = [1, 2, 4]
+	const continuous = continuousArray(array)
+
+	expect(continuous[0]).toBe(1)
+	expect(continuous[0.5]).toBe(1.5)
+	expect(continuous[1.5]).toBe(3)
+	expect(continuous[2]).toBe(4)
+
+	expect(continuous[2.5]).toBeUndefined()
+	expect(continuous[-1]).toBeUndefined()
+})
+
+test('array properties preserved', () => {
+	const array = [1, 2, 4]
+	const continuous = continuousArray(array)
+
+	expect(continuous).toEqual([1, 2, 4])
+	expect(continuous.length).toBe(3)
+
+	continuous.push(0)
+
+	expect(continuous).toEqual([1, 2, 4, 0])
+	expect(continuous.length).toBe(4)
+
+	expect([...continuous]).toEqual(continuous)
+
+	const sliced = continuous.slice(1)
+	expect(sliced).toEqual([2, 4, 0])
+
+	continuous.sort()
+	expect(continuous).toEqual([0, 1, 2, 4])
+})
